0276e3ee91782091fdb66f1d7f6a60a11b5c4cdc
jakublaza <jakub.laza23@gmail.com>
2023-10-15 02:35:01 +0200

    adjust packages

--- code.py
+++ code.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 
 
-
 import sklearn.metrics
 import sklearn.model_selection
 import sklearn.pipeline
@@ -27,21 +26,22 @@ data = train.iloc[:, 1:]
 
 
 # Train-test split
-X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(data, target, test_size=0.2,  random_state=42, stratify=target)
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
+    data, target, test_size=0.2,  random_state=42, stratify=target)
 
 
-
-
-scale_val = np.sum(target == 0 )/np.sum(target == 1)
+scale_val = np.sum(target == 0)/np.sum(target == 1)
 
 # Final Pipeline
 transformer = sklearn.preprocessing.MinMaxScaler()
-polynomial = sklearn.preprocessing.PolynomialFeatures(2, include_bias=False, interaction_only=False)
+polynomial = sklearn.preprocessing.PolynomialFeatures(
+    2, include_bias=False, interaction_only=False)
 pca = sklearn.decomposition.PCA(0.9, whiten=True)
-model = xgb.XGBClassifier(objective = 'binary:logistic',
-                           n_estimators = 2500, max_depth = 12, eta = 0.01, gamma = 1, reg_lambda = 2, subsample = 0.9, scale_pos_weight = 1.5) 
+model = xgb.XGBClassifier(objective='binary:logistic',
+                          n_estimators=2500, max_depth=12, eta=0.01, gamma=1, reg_lambda=2, subsample=0.9, scale_pos_weight=1.5)
 
-pipeline = sklearn.pipeline.Pipeline([ ('transformet', transformer), ('polynomial', polynomial), ('pca', pca),   ('model', model)]) 
+pipeline = sklearn.pipeline.Pipeline(
+    [('transformet', transformer), ('polynomial', polynomial), ('pca', pca),   ('model', model)])
 
 model = pipeline.fit(X_train, y_train)
 
@@ -66,14 +66,13 @@ with open(file_path, 'w') as file:
     file.write(bitstring)
 
 
-
 ##############
 ## First CV ##
 ##############
 
-# CV = sklearn.model_selection.GridSearchCV(pipeline, { 'model__n_estimators':[ 10, 100, 1000, 2000, 5000],'model__max_depth':[3, 6, 9, 12, 20] }, 
+# CV = sklearn.model_selection.GridSearchCV(pipeline, { 'model__n_estimators':[ 10, 100, 1000, 2000, 5000],'model__max_depth':[3, 6, 9, 12, 20] },
 #                                      refit = True)
-# model_CV = CV.fit(X_train, y_train) 
+# model_CV = CV.fit(X_train, y_train)
 
 # print(model_CV.best_params_)
 # print(model_CV.best_score_)
@@ -83,7 +82,6 @@ with open(file_path, 'w') as file:
 # print(acc)
 
 
-
 ###################
 ## randomized CV ##
 ###################
@@ -91,8 +89,8 @@ with open(file_path, 'w') as file:
 # hyperparameter_grid = {
 #     'model__n_estimators': [2000, 2500, 3000],
 #     'model__max_depth': [12, 20, 25],
-#     'model__learning_rate': [0.01, 0.1, 0.5], 
-#     'model__gamma': [0, 1, 2, 3, 4], 
+#     'model__learning_rate': [0.01, 0.1, 0.5],
+#     'model__gamma': [0, 1, 2, 3, 4],
 #     'model__reg_lambda' : [0, 1, 2, 3, 4],
 #     'model__subsample' : [0.85, 0.9],
 #     'model__scale_pos_weight' : [1.5, scale_val, 5]
@@ -103,7 +101,7 @@ with open(file_path, 'w') as file:
 # skf = StratifiedKFold(n_splits=folds, shuffle = True, random_state = 42)
 # random_cv = sklearn.model_selection.RandomizedSearchCV(estimator=pipeline,
 #             param_distributions=hyperparameter_grid,
-#             cv=3, 
+#             cv=3,
 #             n_iter=5,
 #             scoring = 'accuracy',
 #             n_jobs = 5, # to run in parrarel
@@ -111,7 +109,7 @@ with open(file_path, 'w') as file:
 #             return_train_score = True,
 #             random_state=42)
 
-# model_rcv = random_cv.fit(X_train, y_train) 
+# model_rcv = random_cv.fit(X_train, y_train)
 
 # print(model_rcv.best_params_)
 # print(model_rcv.best_score_)
@@ -119,12 +117,3 @@ with open(file_path, 'w') as file:
 # predictions = model_rcv.predict(X_test)
 # acc = sklearn.metrics.accuracy_score(y_test, predictions)
 # print(acc)
-
-
-
-
-
-
-
-
-
